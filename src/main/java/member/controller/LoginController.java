3736f494c00f8c5c70ac9302eaca8ac3ffbee202
limseonghyun <sksrpf1126@naver.com>
2024-04-16 10:17:37 +0900

    feat: Login 기능 구현

--- src/main/java/member/controller/LoginController.java
+++ src/main/java/member/controller/LoginController.java
@@ -7,11 +7,20 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.json.simple.JSONObject;
+
+import common.SessionUtil;
+import member.dto.request.MemberLoginRequest;
+import member.dto.response.MemberResponse;
+import member.service.MemberService;
 
 @WebServlet(urlPatterns = "/member/login")
 public class LoginController extends HttpServlet {
 	
 	private final String LOGIN_VIEW_NAME = "/views/member/login.jsp";
+	private final MemberService memberService = new MemberService();
 
 	@Override
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
@@ -20,6 +29,20 @@ public class LoginController extends HttpServlet {
 
 	@Override
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		MemberLoginRequest member = new MemberLoginRequest();
+		member.setLoginId(request.getParameter("loginId"));
+		member.setPassword(request.getParameter("password"));
+		
+		JSONObject jObj = new JSONObject();
+		try {
+			MemberResponse findMember = memberService.login(member);
+			SessionUtil.setSessionMember(request, findMember);
+		}catch(RuntimeException e) {
+			jObj.put("error", e.getMessage());
+		}
+		
+        response.setContentType("application/x-json; charset=utf-8");
+        response.getWriter().print(jObj);
 	}
 
 	
--- src/main/java/member/dto/response/MemberResponse.java
+++ src/main/java/member/dto/response/MemberResponse.java
@@ -1,5 +1,30 @@
 package member.dto.response;
 
-public class MemberResponse {
+import java.time.LocalDateTime;
 
+public class MemberResponse {
+	private int memberId;
+	private String nickname; //닉네임
+	private LocalDateTime createAt; //가입일
+	
+	public int getMemberId() {
+		return memberId;
+	}
+	public void setMemberId(int memberId) {
+		this.memberId = memberId;
+	}
+	public String getNickname() {
+		return nickname;
+	}
+	public void setNickname(String nickname) {
+		this.nickname = nickname;
+	}
+	public LocalDateTime getCreateAt() {
+		return createAt;
+	}
+	public void setCreateAt(LocalDateTime createAt) {
+		this.createAt = createAt;
+	}
+	
+	
 }
