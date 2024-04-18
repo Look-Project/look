ff36f77669fb9953db5dc65b29e1ff738687c66b
jinny <pjiny92@gmail.com>
2024-04-18 20:12:27 +0900

    refactor: auto ocommit 수정

--- src/main/java/common/DBConnectionUtil.java
+++ src/main/java/common/DBConnectionUtil.java
@@ -21,7 +21,6 @@ public class DBConnectionUtil {
             Context context = new InitialContext();
             DataSource dataSource = (DataSource) context.lookup("java:comp/env/jdbc/pool");
             Connection conn = dataSource.getConnection();
-            conn.setAutoCommit(false);
 			return conn;
 		} catch (Exception e) {
 			throw new RuntimeException(e);
