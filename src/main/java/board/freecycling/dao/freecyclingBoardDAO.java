89eac20dbece88f255a5a24053eccf7c660c3f94
ha2way <gusdkfk@naver.com>
2024-04-19 21:53:34 +0900

    refactor: 게시글 불러오기 메소드 구현

--- src/main/java/board/freecycling/dao/freecyclingBoardDAO.java
+++ src/main/java/board/freecycling/dao/freecyclingBoardDAO.java
@@ -7,8 +7,8 @@ import java.util.ArrayList;
 import java.util.List;
 
 import board.freecycling.dto.request.FreecyclingBoardDTO;
+import board.freecycling.dto.response.FreecyclingBoardListViewDTO;
 import common.DBConnectionUtil;
-
 
 
 public class freecyclingBoardDAO {
@@ -16,55 +16,70 @@ public class freecyclingBoardDAO {
 	Connection con = null;
 	PreparedStatement pstmt = null;
 	ResultSet rs = null;
-	
-	//하나의 게시글을 저장하는 메서드 호출
-			public int insertBoard(FreecyclingBoardDTO bdto){
-				con = DBConnectionUtil.getConnection();
-				int res = 0;
-				String c = "C";
-				try {
-				
-					//데이터를 삽입하는 쿼리
-					String sql="insert into board(USER_ID,TITLE,CONTENTS,CATEGORY) values (?,?,?,?)";
-					pstmt = con.prepareStatement(sql);
-					
-					pstmt.setInt(1, 1);
-					pstmt.setString(2, bdto.getTitle());
-					pstmt.setString(3, bdto.getContents());
-					pstmt.setString(4,c);
-					
-					
-					res = pstmt.executeUpdate();
-					
-				} catch (Exception e) {
-					e.printStackTrace();
-				}finally {
-					DBConnectionUtil.close(con, pstmt, rs);
-				}
-				return res;
-			}
+
+	// 하나의 게시글을 저장하는 메서드 호출
+	public int insertBoard(FreecyclingBoardDTO bdto) {
+		con = DBConnectionUtil.getConnection();
+		int res = 0;
+		String c = "C";
+		try {
+
+			// 데이터를 삽입하는 쿼리
+			String sql = "insert into board(USER_ID,TITLE,CONTENTS,CATEGORY) values (?,?,?,?)";
+			pstmt = con.prepareStatement(sql);
+
+			pstmt.setInt(1, 1);
+			pstmt.setString(2, bdto.getTitle());
+			pstmt.setString(3, bdto.getContents());
+			pstmt.setString(4, c);
+
+			res = pstmt.executeUpdate();
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			DBConnectionUtil.close(con, pstmt, rs);
+		}
+		return res;
+	}
+
+	// 전체게시글을 보는 메서드
+
+	  public List<FreecyclingBoardListViewDTO> getAllBoard(){
+		  List<FreecyclingBoardListViewDTO> resListdto = new ArrayList<>();
+		  
+	 
+		  con = DBConnectionUtil.getConnection();
+		  
+		  String sql = "select m.nickname, b.title, bi.img_src, bi.img_name "
+				  +"from member m inner join board b "
+				  + "on m.user_id = b.user_id "
+				  + "left outer join board_img bi "
+				  + "on b.board_id = bi.board_id "
+				  + "where b.category = 'C' and b.delete_yn = 'N' ";
+		  
+	  try {
+	 
+		  pstmt = con.prepareStatement(sql);
+
+		  rs = pstmt.executeQuery();
+	    while(rs.next()) {
+	    	FreecyclingBoardListViewDTO blvdto = new FreecyclingBoardListViewDTO();
+	    	blvdto.setNickname(rs.getString(1));
+	    	blvdto.setTitle(rs.getString(2));
+	    	blvdto.setImg_src(rs.getString(3));
+	    	blvdto.setImg_name(rs.getString(4));
 			
-	//전체게시글을 보는 메서드
-	/*
-	 * public List<FreecyclingBoardDTO> getAllBoard{ List<FreecyclingBoardDTO> v =
-	 * new ArrayList<>();
-	 * 
-	 * try {
-	 * 
-	 * String sql="select * from  (select * from board" +"where category = C";
-	 * pstmt=con.prepareStatement(sql);
-	 * 
-	 * rs = pstmt.executeQuery();
-	 * 
-	 * while(rs.next()) { BoardBean bean = new BoardBean();
-	 * bean.setNum(rs.getInt(1)); bean.setWriter(rs.getString(2));
-	 * bean.setEmail(rs.getString(3)); bean.setSubject(rs.getString(4));
-	 * bean.setPassword(rs.getString(5));
-	 * bean.setReg_date(rs.getDate(6).toString()); bean.setRef(rs.getInt(7));
-	 * bean.setRe_step(rs.getInt(8)); bean.setRe_level(rs.getInt(9));
-	 * bean.setReadcount(rs.getInt(10)); bean.setContent(rs.getString(11)); //패키징한
-	 * 데이터를 벡터에 저장 v.add(bean); } con.close(); } catch (Exception e) {
-	 * e.printStackTrace(); } return v; }
-	 * 
-	 */	
+			//패키징한 데이터를 List에 저장
+	    	resListdto.add(blvdto);
+		}
+		
+	  } catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			DBConnectionUtil.close(con, pstmt, rs);
+		}
+	   return resListdto; 
+	   
+	  }
 }
--- src/main/java/board/freecycling/service/FreecyclingBoardService.java
+++ src/main/java/board/freecycling/service/FreecyclingBoardService.java
@@ -1,13 +1,25 @@
 package board.freecycling.service;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import board.freecycling.dao.freecyclingBoardDAO;
 import board.freecycling.dto.request.FreecyclingBoardDTO;
+import board.freecycling.dto.response.FreecyclingBoardListViewDTO;
 
 public class FreecyclingBoardService {
 	
 	freecyclingBoardDAO bdao = new freecyclingBoardDAO();
 	
-	public void freecyclingServicerequest(FreecyclingBoardDTO bdto) {
+	//게시글 작성 후 데이터베이스 입력 메소드
+	public void freecyclingInsertService(FreecyclingBoardDTO bdto) {
 		bdao.insertBoard(bdto);
 	}
+	
+	
+	//게시글 리스트 출력 메소드
+	public List<FreecyclingBoardListViewDTO> freecyclingListViewService() { 
+		return bdao.getAllBoard();
+	}
+	
 }
