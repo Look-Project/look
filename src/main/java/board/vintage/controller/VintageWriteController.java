dc3a106118783d3bc51e08c1a2c1738f74e0baac
jinny <pjiny92@gmail.com>
2024-04-21 22:46:16 +0900

    feat: add session for maintain login

--- src/main/java/board/vintage/controller/VintageWriteController.java
+++ src/main/java/board/vintage/controller/VintageWriteController.java
@@ -10,15 +10,23 @@ import javax.servlet.http.HttpServletResponse;
 
 import board.vintage.dto.request.VintageWriteRequest;
 import board.vintage.service.VintageBoardService;
+import common.SessionUtil;
 
 @WebServlet(urlPatterns = "/vintage/write")
 public class VintageWriteController extends HttpServlet {
 	private final String VINTAGE_BOARD_WRITE = "/views/board/vintage/vintagewriteform.jsp";
+	private final String LOGIN = "/views/member/login.jsp";
+	private final String VINTAGE_BOARD_LIST = "/vintage/boardlist";
 	VintageBoardService vbs = new VintageBoardService();
 	
 	@Override
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		if(SessionUtil.getSessionMember(request) != null) {
 		request.getRequestDispatcher(VINTAGE_BOARD_WRITE).forward(request, response);
+		}else {
+			request.getRequestDispatcher(LOGIN).forward(request, response);
+		}
+		
 	}
 
 	@Override
@@ -26,10 +34,11 @@ public class VintageWriteController extends HttpServlet {
 		
 		VintageWriteRequest vwr = new VintageWriteRequest();
 		
-		vwr.setTitle(request.getParameter("title"));;
+		vwr.setTitle(request.getParameter("title"));
 		vwr.setContents(request.getParameter("contents"));
-		
+		vwr.setMemberId(SessionUtil.getSessionMember(request).getMemberId());
 		vbs.post(vwr);
+		response.sendRedirect(request.getContextPath() + VINTAGE_BOARD_LIST);
 	}
 	
 }
