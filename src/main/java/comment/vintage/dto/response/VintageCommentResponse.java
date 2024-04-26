254a5150874a348a81d9404d5ff4b46ec5ef5d0e
jinny <pjiny92@gmail.com>
2024-04-26 21:56:46 +0900

    feat: Vintage Comment 삭제 기능 구현

--- src/main/java/comment/vintage/dto/response/VintageCommentResponse.java
+++ src/main/java/comment/vintage/dto/response/VintageCommentResponse.java
@@ -1,16 +1,36 @@
 package comment.vintage.dto.response;
 
 public class VintageCommentResponse {
-
+	
+	private int commentId;
+	
+	private int memberId;
     private String nickName;
+    
     private String comment;
 
+    
+    
+	public int getCommentId() {
+		return commentId;
+	}
+	public void setCommentId(int commentId) {
+		this.commentId = commentId;
+	}
+	
+	public int getMemberId() {
+		return memberId;
+	}
+	public void setMemberId(int memberId) {
+		this.memberId = memberId;
+	}
 	public String getNickName() {
 		return nickName;
 	}
 	public void setNickName(String nickName) {
 		this.nickName = nickName;
 	}
+	
 	public String getComment() {
 		return comment;
 	}
--- src/main/java/comment/vintage/service/VintageCommentService.java
+++ src/main/java/comment/vintage/service/VintageCommentService.java
@@ -22,4 +22,8 @@ public class VintageCommentService {
     public List<VintageCommentResponse> getAllComments(int boardId) {
         return cDAO.getAllComments(boardId);
     }
+    
+    public int setDeleteComment(int commentId) {
+    	return cDAO.setDeleteComment(commentId);
+    }
 }
